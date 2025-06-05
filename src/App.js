84821d1fcd341713b9c2381008b5e465802d6e44
Krishna Kumari Simha <simhakrishnakumari29@gmail.com>
2025-06-05 10:58:41 +0530

    Updated App.js with welcome message and button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { SpeedInsights } from "@vercel/speed-insights/react";;
-import './App.css';
+import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-<SpeedInsights />
 function App() {
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Welcome to HireWithKrishna!</h1>
-      {/* Other components */}
-
+      <p>We offer resume writing services.</p>
+      <button
+        onClick={() => alert("Thank you for showing interest!")}
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          backgroundColor: "#4CAF50",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer"
+        }}
+      >
+        Get Started
+      </button>
       <SpeedInsights />
     </div>
   );
 }
+
+export default App;
 export default App;
