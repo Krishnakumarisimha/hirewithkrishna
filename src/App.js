d6739c64159cf6b47aaa0a5d4e8170f5b24b4b0d
Krishna Kumari Simha <simhakrishnakumari29@gmail.com>
2025-06-05 14:54:15 +0530

    Build completed and project ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from 
+"@vercel/speed-insights/react";
 
 function App() {
   return (
@@ -12,4 +13,4 @@ function App() {
   );
 }
 
-export default App;export default App;
+export default App;
