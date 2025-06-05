e9550e15c4958bc3eed7b14c534317824ab0a072
Krishna Kumari Simha <simhakrishnakumari29@gmail.com>
2025-06-05 11:45:09 +0530

    Added Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div>
       <h1>Welcome to HireWithKrishna!</h1>
       <p>We offer resume writing services.</p>
-      <button
-        onClick={() => alert("Thank you for showing interest!")}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          backgroundColor: "#4CAF50",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer"
-        }}
-      >
-        Get Started
-      </button>
+      <button>Get Started</button>
+
       <SpeedInsights />
     </div>
   );
 }
 
-export default App;
-export default App;
+export default App;export default App;
